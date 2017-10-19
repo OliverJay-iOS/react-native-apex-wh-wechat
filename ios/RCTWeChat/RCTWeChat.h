217bdcc71393b01bae220f53bceb2a714659669b
鲁利杰 <lulijie@lulijiedeMacBook-Pro.local>
2017-10-19 17:57:22 +0800

    First commit

---
+++ ios/RCTWeChat/RCTWeChat.h
@@ -0,0 +1,39 @@
+//
+//  RCTWeChat.h
+//  RCTWeChat
+//
+//  Created by 鲁利杰 on 2017/10/19.
+//  Copyright © 2017年 鲁利杰. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#import <React/RCTBridgeModule.h>
+#import "WXApi.h"
+
+// define share type constants
+#define RCTWXShareTypeNews @"news"
+#define RCTWXShareTypeThumbImageUrl @"thumbImage"
+#define RCTWXShareTypeImageUrl @"imageUrl"
+#define RCTWXShareTypeImageFile @"imageFile"
+#define RCTWXShareTypeImageResource @"imageResource"
+#define RCTWXShareTypeText @"text"
+#define RCTWXShareTypeVideo @"video"
+#define RCTWXShareTypeAudio @"audio"
+#define RCTWXShareTypeFile @"file"
+
+#define RCTWXShareType @"type"
+#define RCTWXShareTitle @"title"
+#define RCTWXShareDescription @"description"
+#define RCTWXShareWebpageUrl @"webpageUrl"
+#define RCTWXShareImageUrl @"imageUrl"
+
+#define RCTWXEventName @"WeChat_Resp"
+
+@interface RCTWeChat : NSObject <RCTBridgeModule, WXApiDelegate>
+
+@property NSString* appId;
+
+@end
+
